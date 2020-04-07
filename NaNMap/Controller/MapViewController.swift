04e3776524ff3979a3c0a9ef9e9b816bb96a6e52
oota <shark747474@gmail.com>
2020-04-07 22:33:56 +0900

    add MapViewController

---
+++ NaNMap/Controller/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/04/07.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+private extension MapViewController {
+    
+}
