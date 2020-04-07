2266c2bea9467d5c5c94d8dbbd3b980232fe3342
oota <shark747474@gmail.com>
2020-04-07 20:08:00 +0900

    Initial Commit

---
+++ NaNMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/04/07.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
