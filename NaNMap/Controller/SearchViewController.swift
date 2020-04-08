4ec8b1251e8f8a084e4e45186862f47502f8324b
oota <shark747474@gmail.com>
2020-04-09 04:07:45 +0900

    udpate SearchViewController

--- NaNMap/Controller/SearchViewController.swift
+++ NaNMap/Controller/SearchViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    var searchBar: UISearchBar!
+    
     static func instantinate() -> SearchViewController {
         return UIStoryboard(name: "Search", bundle: nil).instantiateInitialViewController() as! SearchViewController
     }
@@ -24,5 +26,18 @@ private extension SearchViewController {
     
     func setUpNavigationBar() {
         navigationItem.hidesBackButton = true
+        setUpSearchBar()
+    }
+    
+    func setUpSearchBar() {
+        if let navigationBarFrame = navigationController?.navigationBar.bounds {
+            let searchBar = UISearchBar(frame: navigationBarFrame)
+            searchBar.placeholder = "場所や目的地を検索"
+            searchBar.searchTextField.backgroundColor = UIColor.white
+            searchBar.tintColor = UIColor.gray
+            searchBar.keyboardType = UIKeyboardType.default
+            navigationItem.titleView = searchBar
+            self.searchBar = searchBar
+        }
     }
 }
