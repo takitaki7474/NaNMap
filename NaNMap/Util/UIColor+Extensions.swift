df86bfbfeabf3f0076f1b8c3a5b4fbe7b288d75a
oota <shark747474@gmail.com>
2020-04-08 20:08:11 +0900

    add Util

---
+++ NaNMap/Util/UIColor+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/04/08.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgba(red: Int, green: Int, blue: Int) -> UIColor {
+        return UIColor(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: 1.0
+        )
+    }
+}
