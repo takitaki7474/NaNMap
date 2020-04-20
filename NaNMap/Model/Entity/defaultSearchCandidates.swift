16dd89bafa1132f07e9d39e50a3bca54414f79dd
oota <shark747474@gmail.com>
2020-04-20 18:44:17 +0900

    add defaultSearchCandidates entity

---
+++ NaNMap/Model/Entity/defaultSearchCandidates.swift
@@ -0,0 +1,38 @@
+//
+//  defaultSearchCandidates.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/04/20.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultSearchCandidate: Codable {
+    var section: [Section]
+}
+
+struct Section: Codable {
+    var row: [Row]
+}
+
+struct Row: Codable {
+    var title: String
+    var nextPage: [BuildingInfo]
+}
+
+struct BuildingInfo: Codable {
+    var building: String
+    var coordinate: Coordinate
+    var facility: [FacilityInfo]?
+}
+
+struct Coordinate: Codable {
+    var longitude: Float
+    var latitude: Float
+}
+
+struct FacilityInfo: Codable {
+    var facilityName: String
+    var floor: String
+}
