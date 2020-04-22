6085c36376834019250b6e5d590574b2ec142480
oota <shark747474@gmail.com>
2020-04-23 04:07:31 +0900

    update TimeTableViewController

--- NaNMap/Controller/TimeTableViewController.swift
+++ NaNMap/Controller/TimeTableViewController.swift
@@ -17,8 +17,21 @@ class TimeTableViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-        collectionView.dataSource = self
         super.viewDidLoad()
+        setUpCollectionView()
+    }
+}
+
+
+private extension TimeTableViewController {
+    
+    func setUpCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 2.0
+        layout.minimumInteritemSpacing = 2.0
+        collectionView.collectionViewLayout = layout
     }
 }
 
@@ -26,12 +39,43 @@ class TimeTableViewController: UIViewController {
 extension TimeTableViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 42
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timeTableCell", for: indexPath)
         cell.backgroundColor = .red
         return cell
+    }
+}
+
+
+extension TimeTableViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let viewWidth = collectionView.frame.width
+        let viewHeight = collectionView.frame.height
+        let firstCellOneSideSize: CGFloat = 30.0
+        let scheduleCellWidth = (viewWidth - firstCellOneSideSize - 2*6) / 6.0
+        let scheduleCellHeight = (viewHeight - firstCellOneSideSize - 2*5) / 5.0
+        var width: CGFloat?
+        var height: CGFloat?
+        
+        switch indexPath.row {
+        case 0:
+            width = firstCellOneSideSize
+            height = firstCellOneSideSize
+        case 1...6:
+            width = scheduleCellWidth
+            height = firstCellOneSideSize
+        case let index where index % 7 == 0:
+            width = firstCellOneSideSize
+            height = scheduleCellHeight
+        default:
+            width = scheduleCellWidth
+            height = scheduleCellHeight
+        }
+        
+        return CGSize(width: width!, height: height!)
     }
 }
---
+++ NaNMap/Util/CustomCellFlowLayout.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCellFlowLayout.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/04/23.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
