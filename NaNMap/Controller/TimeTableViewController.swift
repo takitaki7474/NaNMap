88a418e480079aeeca956bd944236d52ed5c81d5
oota <shark747474@gmail.com>
2020-04-23 01:14:58 +0900

    update TimeTableViewController

--- NaNMap/Controller/TimeTableViewController.swift
+++ NaNMap/Controller/TimeTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TimeTableViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     static func instantinate() -> TimeTableViewController {
          return UIStoryboard(name: "TimeTable", bundle: nil).instantiateViewController(withIdentifier: "timeTableViewController") as! TimeTableViewController
     }
@@ -17,5 +19,18 @@ class TimeTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+
+extension TimeTableViewController: UICollectionViewDataSource {
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timeTableCell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
