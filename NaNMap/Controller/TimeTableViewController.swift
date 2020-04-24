0ca928ec9f05ee98ab65e1fda8517da124d6e115
oota <shark747474@gmail.com>
2020-04-24 23:33:18 +0900

    add alert

--- NaNMap/Controller/TimeTableViewController.swift
+++ NaNMap/Controller/TimeTableViewController.swift
@@ -45,7 +45,27 @@ private extension TimeTableViewController {
 }
 
 
-extension TimeTableViewController: UICollectionViewDataSource {
+private extension TimeTableViewController {
+    
+    func alertChangeTimeTable(indexPath: IndexPath) {
+        let dayOfWeek: [String] = ["月", "火", "水", "木", "金", "土"]
+        let period: [String] = ["1", "2", "3", "4", "5"]
+        let clickedCellText = dayOfWeek[(indexPath.row % 7) - 1] + period[(indexPath.row / 7) - 1]
+        let alert = UIAlertController(title: "時間割の編集", message: clickedCellText + "の講義を編集しますか?", preferredStyle: UIAlertController.Style.actionSheet)
+        let defaultAction = UIAlertAction(title: clickedCellText + "の講義を検索する", style: UIAlertAction.Style.default, handler: {
+            (action:UIAlertAction) -> Void in
+        })
+        let cancelAction = UIAlertAction(title: "戻る", style: UIAlertAction.Style.cancel, handler: {
+            (action: UIAlertAction) -> Void in
+        })
+        alert.addAction(defaultAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+
+extension TimeTableViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 42
@@ -57,6 +77,12 @@ extension TimeTableViewController: UICollectionViewDataSource {
         cell.layer.cornerRadius = 4
         customCellCreator.customizeCellLabelFlowLayout(indexPath: indexPath, cell: cell)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if !(indexPath.row <= 6 || indexPath.row % 7 == 0) {
+            alertChangeTimeTable(indexPath: indexPath)
+        }
     }
 }
 
