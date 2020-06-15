c6f42d93dabca46bf18717abec6e9551f6a78e24
oota <shark747474@gmail.com>
2020-06-15 21:32:36 +0900

    add MapViewPresenter

--- NaNMap/Presenter/MapViewPresenter.swift
+++ NaNMap/Presenter/MapViewPresenter.swift
@@ -5,5 +5,25 @@
 //  Created by 太田龍之介 on 2020/06/15.
 //  Copyright © 2020 ryunosuke ota. All rights reserved.
 //
+import MapKit
 
-import Foundation
+final class MapViewPresenter {
+    weak var view: MapView?
+    private let model = MapModel()
+    
+    init(view: MapView) {
+        self.view = view
+        model.delegate = self
+    }
+    
+    func setUpMapRegion() {
+        model.setUpMapRegion()
+    }
+}
+
+extension MapViewPresenter: MapModelDelegate {
+    
+    func reloadRegion(at region: MKCoordinateRegion) {
+        view?.reloadRegion(at: region)
+    }
+}
