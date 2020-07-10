f8d776e7a51c3f05c2062f657f62c313aa4b64c5
oota <shark747474@gmail.com>
2020-07-11 00:43:24 +0900

    Add loadTimeTableCell function to TimeTableModel

--- NaNMap/Sources/UI/TimeTable/TimeTableModel.swift
+++ NaNMap/Sources/UI/TimeTable/TimeTableModel.swift
@@ -5,6 +5,15 @@
 //  Created by 太田龍之介 on 2020/06/19.
 //  Copyright © 2020 ryunosuke ota. All rights reserved.
 //
+import RealmSwift
+
+class TimeTableCellObj: Object {
+    @objc dynamic var category = ""
+    @objc dynamic var semester = ""
+    @objc dynamic var subjectName = ""
+    @objc dynamic var teacher = ""
+    @objc dynamic var classroom = ""
+}
 
 protocol TimeTableModelDelegate: class {
     func alertWillSearchSyllabus(with text: String)
@@ -12,6 +21,31 @@ protocol TimeTableModelDelegate: class {
 
 class TimeTableModel {
     weak var delegate: TimeTableModelDelegate?
+    var timeTableCell: Results<TimeTableCellObj>? {
+        didSet {
+            
+        }
+    }
+    
+    init() {
+        loadTimeTableCell()
+    }
+    
+    private func loadTimeTableCell() {
+        let realm = try! Realm()
+        if realm.objects(TimeTableCellObj.self).count == 0 { saveTimeTableCell(realm: realm) }
+        self.timeTableCell = realm.objects(TimeTableCellObj.self)
+    }
+    
+    private func saveTimeTableCell(realm: Realm) {
+        try! realm.write {
+            for _ in 0...42 {
+                let timeTableCellObj = TimeTableCellObj()
+                realm.add(timeTableCellObj)
+            }
+        }
+        print("save TimeTableCellObj on realm")
+    }
     
     func setAlertText(at index: Int) {
         let week: [String] = ["月", "火", "水", "木", "金", "土"]
