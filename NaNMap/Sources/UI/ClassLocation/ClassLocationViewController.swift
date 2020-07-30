025fc08fbb75d2e25831304d8a49d3181a9f09c8
oota <shark747474@gmail.com>
2020-07-30 22:32:38 +0900

    Add ClassLocationViewController to TimeTable Storyboard

---
+++ NaNMap/Sources/UI/ClassLocation/ClassLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ClassLocationViewController.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/07/30.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import UIKit
+
+class ClassLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
