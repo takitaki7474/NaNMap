e886590d301df5c8295ec668f4fe4e3b69a81bb4
oota <shark747474@gmail.com>
2020-08-01 00:46:59 +0900

    Add ClassInformationView

---
+++ NaNMap/Sources/UI/ClassLocation/ClassInformationView.swift
@@ -0,0 +1,14 @@
+//
+//  ClassInformationView.swift
+//  NaNMap
+//
+//  Created by 太田龍之介 on 2020/08/01.
+//  Copyright © 2020 ryunosuke ota. All rights reserved.
+//
+
+import UIKit
+
+class ClassInformationView: UIView {
+
+
+}
