02aeafbc7d4495e92530ab047741d7cbe3ccce69
oota <shark747474@gmail.com>
2020-08-02 23:03:39 +0900

    Update ClassInformation style

--- NaNMap/Sources/UI/ClassLocation/ClassInformationView.swift
+++ NaNMap/Sources/UI/ClassLocation/ClassInformationView.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ClassInformationView: UIView {
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var courseItemLabel: UILabel!
+    @IBOutlet weak var locationItemLabel: UILabel!
+    @IBOutlet weak var teacherItemLabel: UILabel!
+    @IBOutlet weak var categoryItemLabel: UILabel!
+    @IBOutlet weak var semesterItemLabel: UILabel!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,8 +34,25 @@ class ClassInformationView: UIView {
     
     private func setViewStyle(view: UIView) -> UIView {
         view.frame = self.frame
-        view.backgroundColor = .white
-        testLabel.center = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
+        view.backgroundColor = .blue
+        let lineSpace: CGFloat = 5.0
+        let itemHeight: CGFloat = (view.frame.height - (lineSpace * 6.0)) / 5.0
+        let itemWidth: CGFloat = 70.0
+        let itemSize = CGSize(width: itemWidth, height: itemHeight)
+        setItemStyle(label: courseItemLabel, point: CGPoint(x: lineSpace, y: lineSpace), size: itemSize, text: "Course")
+        setItemStyle(label: locationItemLabel, point: CGPoint(x: lineSpace, y: (lineSpace*2)+itemHeight), size: itemSize, text: "Location")
+        setItemStyle(label: teacherItemLabel, point: CGPoint(x: lineSpace, y: (lineSpace*3)+(itemHeight*2)), size: itemSize, text: "Teacher")
+        setItemStyle(label: categoryItemLabel, point: CGPoint(x: lineSpace, y: (lineSpace*4)+(itemHeight*3)), size: itemSize, text: "Category")
+        setItemStyle(label: semesterItemLabel, point: CGPoint(x: lineSpace, y: (lineSpace*5)+(itemHeight*4)), size: itemSize, text: "Semester")
+        
         return view
+    }
+    
+    private func setItemStyle(label: UILabel, point: CGPoint, size: CGSize, text: String) {
+        label.frame.origin = point
+        label.frame.size = size
+        label.backgroundColor = .gray
+        label.font = UIFont.systemFont(ofSize: 12.0)
+        label.text = text
     }
 }
