f4dd53357be5ec6b51b03a7dcf18d4f60ce345e4
oota <shark747474@gmail.com>
2020-08-08 21:19:13 +0900

    Change MapBuildingObj and MapFacilityObj to MapFacilityObj only

--- NaNMap/Sources/UI/MapSearch/MapSearchModel.swift
+++ NaNMap/Sources/UI/MapSearch/MapSearchModel.swift
@@ -9,19 +9,15 @@
 import Foundation
 import RealmSwift
 
-class MapBuildingObj: Object {
-    @objc dynamic var building = ""
-    @objc dynamic var longitude = 0.0
-    @objc dynamic var latitude = 0.0
-    let facilities = List<MapFacilityObj>()
-    override static func primaryKey() -> String? {
-        return "building"
-    }
-}
-
 class MapFacilityObj: Object {
     @objc dynamic var facilityName = ""
     @objc dynamic var floor = ""
+    @objc dynamic var building = ""
+    @objc dynamic var longitude = 0.0
+    @objc dynamic var latitude = 0.0
+    override static func primaryKey() -> String? {
+        return "facilityName"
+    }
 }
 
 protocol MapSearchModelDelegate: class {
@@ -31,7 +27,7 @@ protocol MapSearchModelDelegate: class {
 final class MapSearchModel {
     weak var delegate: MapSearchModelDelegate?
     var buildings: [Building]?
-    var facilitySearchResults: Results<MapBuildingObj>? {
+    var facilitySearchResults: Results<MapFacilityObj>? {
         didSet {
             delegate?.searchModel()
         }
@@ -50,40 +46,39 @@ final class MapSearchModel {
             return
         }
         self.buildings = buildings
-        saveMapBuildingObj(buildings: buildings)
+        saveMapFacilityObj(buildings: buildings)
     }
     
-    private func saveMapBuildingObj(buildings: [Building]) {
+    private func saveMapFacilityObj(buildings: [Building]) {
         let realm = try! Realm()
-        if realm.objects(MapBuildingObj.self).count == 0 {
+        if realm.objects(MapFacilityObj.self).count == 0 {
             try! realm.write {
                 for building in buildings {
-                    let mapBuildingObj = MapBuildingObj()
-                    mapBuildingObj.building = building.building
-                    mapBuildingObj.longitude = building.coordinate.longitude
-                    mapBuildingObj.latitude = building.coordinate.latitude
                     if let facilities = building.facilities {
                         for facility in facilities {
                             let mapFacilityObj = MapFacilityObj()
                             mapFacilityObj.facilityName = facility.facilityName
                             mapFacilityObj.floor = facility.floor
-                            mapBuildingObj.facilities.append(mapFacilityObj)
+                            mapFacilityObj.building = building.building
+                            mapFacilityObj.longitude = building.coordinate.longitude
+                            mapFacilityObj.latitude = building.coordinate.latitude
+                            realm.add(mapFacilityObj)
                         }
                     }
-                    realm.add(mapBuildingObj)
                 }
+                print("save MapFacilityObj on realm")
             }
-            print("save MapBuildingObj on realm")
         }
+        print(realm.objects(MapFacilityObj.self))
     }
 }
 
 extension MapSearchModel {
     func searchFacility(with query: String) {
         let realm = try! Realm()
-        var result = realm.objects(MapBuildingObj.self).filter("facilities.@count >= 1")
+        var result = realm.objects(MapFacilityObj.self)
         let buildingPredicate = NSPredicate(format: "building CONTAINS %@", argumentArray: [query])
-        let facilityNamePredicate = NSPredicate(format: "SUBQUERY(facilities, $facility, $facility.facilityName CONTAINS %@).@count >= 1", argumentArray: [query])
+        let facilityNamePredicate = NSPredicate(format: "facilityName CONTAINS %@", argumentArray: [query])
         let predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [buildingPredicate, facilityNamePredicate])
         result = result.filter(predicate)
         self.facilitySearchResults = result
