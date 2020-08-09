f3c762b0124772f94ec9d23dfb45b6e42fe3f050
oota <shark747474@gmail.com>
2020-08-09 15:17:19 +0900

    Add UIColor alpha extension

--- NaNMap/Sources/Common/Extension/UIColor+Extensions.swift
+++ NaNMap/Sources/Common/Extension/UIColor+Extensions.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 extension UIColor {
-    static func rgba(red: Int, green: Int, blue: Int) -> UIColor {
+    static func rgba(red: Int, green: Int, blue: Int, alpha: Float = 1.0) -> UIColor {
         return UIColor(
             red: CGFloat(red) / 255.0,
             green: CGFloat(green) / 255.0,
             blue: CGFloat(blue) / 255.0,
-            alpha: 1.0
+            alpha: CGFloat(alpha)
         )
     }
 }
